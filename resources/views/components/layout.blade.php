ed1b218b8a091e5d484d79744770435f2eebfefe
Less <31947204+lessandsass@users.noreply.github.com>
2024-07-08 00:34:56 +0530

    layout file was created
    logo component was added.
    welcome view was grabbed from the layout file.

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Remote-Jobs</title>
+</head>
+<body>
+
+    <div>
+
+        <nav>
+            <div>
+                <a href="">
+                    <x-logo />
+                </a>
+            </div>
+
+            <div>
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+
+            <div>
+                <a href="#">Post a job</a>
+            </div>
+        </nav>
+
+        <main>
+
+        </main>
+
+    </div>
+
+</body>
+</html>
