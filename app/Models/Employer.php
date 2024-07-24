434b82b2c69c0f2f4d11cde5b2f49498b148899f
Less <31947204+lessandsass@users.noreply.github.com>
2024-07-24 07:30:35 +0530

    Relationships
    Relationships were added to the models

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Employer extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
+
 }
